93cc80f474b9ba772d3ad9bd5da8c53933e6fa5e
rzambrano2 <rzambrano2@6c3ccbe3-c8f8-447c-8310-d8bfdb2af133>
2013-06-18 19:18:34 +0000

    CodeIgniter

--- trunk/CodeIgniter_2.1.3/application/config/database.php
+++ trunk/CodeIgniter_2.1.3/application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'daweb_krd';
+$db['default']['password'] = 'krd';
+$db['default']['database'] = 'daweb_krd';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
