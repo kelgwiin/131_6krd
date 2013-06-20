19b2253e44f40293613afd87a1b9d68d726ba03d
rzambrano2 <rzambrano2@6c3ccbe3-c8f8-447c-8310-d8bfdb2af133>
2013-06-20 16:57:53 +0000

    routes

--- trunk/CodeIgniter_2.1.3/application/config/routes.php
+++ trunk/CodeIgniter_2.1.3/application/config/routes.php
@@ -38,17 +38,17 @@
 |
 */
 
-
+/**
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
 $route['default_controller'] = 'pages/view';
+**/
 
+$route['default_controller'] = "welcome";
+$route['404_override'] = '';
 
-//$route['default_controller'] = "welcome";
-//$route['404_override'] = '';
-
-//$route['test'] = "welcometest/indextest";
+$route['test'] = "welcometest/indextest";
 
 
 /* End of file routes.php */
