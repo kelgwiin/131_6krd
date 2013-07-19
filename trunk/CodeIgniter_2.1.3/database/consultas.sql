817886167cc982c17947a5117d7d6743228e9d13
kelwin_gamez <kelwin_gamez@6c3ccbe3-c8f8-447c-8310-d8bfdb2af133>
2013-07-19 16:17:16 +0000

    agregado el modelo droche.php

---
+++ trunk/CodeIgniter_2.1.3/database/consultas.sql
@@ -0,0 +1,44 @@
+--~ CONSULTAS DE BASE DE DATOS DE HOTES D'ROCHE
+
+
+--:: PARA RESERVAR::
+--Número de habitaciones de un tipo, no se toma en cuenta el tipo porque
+--en el proyecto se puede hacer una excepción de una individual por una doble
+
+-->>>INDIVIDUAL
+SELECT count(*)
+FROM habitaciones
+WHERE categoria = '?';
+
+-- Número de habitaciones reservadas para una fecha dada
+SELECT count(*)
+FROM reserva_ocupa
+WHERE '?_ini' >= fecha_ini AND '?_ini' <= fecha_ini 
+AND '?_fin' >= fecha_fin AND '?_fin' <= fecha_fin
+AND categoria_habitacion = '?'  AND estatus_reserva != 'cancelada';
+
+-->>>DOBLES
+SELECT count(*)
+FROM habitaciones
+WHERE categoria = '?' AND tipo = '2';
+
+-- Número de habitaciones reservadas para una fecha dada
+SELECT count(*)
+FROM reserva_ocupa
+WHERE '?_ini' >= fecha_ini AND '?_ini' <= fecha_ini 
+AND '?_fin' >= fecha_fin AND '?_fin' <= fecha_fin
+AND categoria_habitacion = '?'  AND tipo = '2' 
+AND estatus_reserva != 'cancelada';
+
+--~ CODIGO habitaciones totales con reservadas_ocupadas
+--~ if(total_hab  - total_reserva > 0)
+--~ ... Proceder
+
+--:: OCUPAR Habitación  -- previa RESERVA:::
+
+
+
+
+
+
+
