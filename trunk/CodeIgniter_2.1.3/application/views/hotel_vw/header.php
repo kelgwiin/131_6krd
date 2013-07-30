b98946d42b81ceb677580709ef7e76b9572c98e4
kelwin_gamez <kelwin_gamez@6c3ccbe3-c8f8-447c-8310-d8bfdb2af133>
2013-07-30 03:32:03 +0000

    datatble

--- trunk/CodeIgniter_2.1.3/application/views/hotel_vw/header.php
+++ trunk/CodeIgniter_2.1.3/application/views/hotel_vw/header.php
@@ -31,9 +31,35 @@
   <link rel="stylesheet" href="application/views/hotel_vw/config/DataTables-1.9.4/media/css/jquery.dataTables_themeroller.css">
   <!--  <link rel="stylesheet" href="application/views/hotel_vw/config/DataTables-1.9.4/media/css/demo_table.css">-->
   <!--<link rel="stylesheet" href="application/views/hotel_vw/config/DataTables-1.9.4/media/css/demo_table_jui.css">-->
-  <link rel="stylesheet" href="application/views/hotel_vw/config/DataTables-1.9.4/extras/TableTools/media/css/TableTools_JUI.css">
-  <!-- Fin de DateTable -->
+  <!--<link rel="stylesheet" href="application/views/hotel_vw/config/DataTables-1.9.4/extras/TableTools/media/css/TableTools_JUI.css"> -->
+  <!-- Fin de DateTable - CSS-->
 
+<!-- JavaScript-->
+<script>
+  document.write('<script src=' +
+  ('__proto__' in {} ? 'js/vendor/zepto' : 'js/vendor/jquery') +
+  '.js><\/script>')
+  </script>
+  
+  
+  
+ 	<script src="application/views/hotel_vw/config/js/jquery-1.9.1.js" type="text/javascript"></script> 
+	<link rel="stylesheet" href="application/views/hotel_vw/config/css/ui-lightness/jquery-ui.custom.css" />
+	<script src="application/views/hotel_vw/config/js/jquery-ui-1.10.3.custom.js"></script>
+	<script src="application/views/hotel_vw/config/js/jquery-ui-1.10.3.custom.min.js"></script>
+	
+        <!-- DateTable-->
+        <script src="application/views/hotel_vw/config/DataTables-1.9.4/media/js/jquery.dataTables.js"></script>
+        <script src="application/views/hotel_vw/config/DataTables-1.9.4/media/js/jquery.dataTables.min.js"></script>
+        <!--<script src="application/views/hotel_vw/config/DataTables-1.9.4/media/js/jquery.js"></script>-->
+        
+        <!--<script src="application/views/hotel_vw/config/DataTables-1.9.4/extras/TableTools/media/js/TableTools.js"></script>
+        <script src="application/views/hotel_vw/config/DataTables-1.9.4/extras/TableTools/media/js/ZeroClipboard.js"></script>-->
+        
+        <!-- Fin de DateTable-->
+        
+	<script src="application/views/hotel_vw/config/js/app.js" type="text/javascript"></script>
+	
   <link rel="icon"   type="image/x-icon"  
 	href="application/views/hotel_vw/config/img/favicon.ico" /> 
   
